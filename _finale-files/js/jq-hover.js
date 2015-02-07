042325d8899d31ff74090c0112ad55d9a3b16343
Evan Johnson <evan@createinform.com>
2015-02-07 08:30:11 -0800

    Add exercise 10.

---
+++ _finale-files/js/jq-hover.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+  // statement here
+  $("#button").hover(function() {
+    // hover in statement
+    console.log("hovering button");
+  }, function() {
+    // hover out statement
+    console.log("not hovering button");
+  });
+});
